61fd53726bd5057205ffa7d8a2b11dfe28af743f
ludeptrai <luuphan2810@gmail.com>
2020-03-20 19:29:33 +0700

    ec

--- bots/adaptive_cards_bot.py
+++ bots/adaptive_cards_bot.py
@@ -53,13 +53,12 @@ import requests
 #     return {'global':global_statistic,'vietnam':vietnam_statistic}
 
 def get_api():
-    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = GOOGLE_CHROME_PATH
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     # driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
     url = "http://ncov.moh.gov.vn"
     driver.get(url)
